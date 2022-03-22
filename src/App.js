4ceea3aad3fe72120fc2fb741146c573e6daf5ce
Kishore Kumar <kishore1234500000@gmail.com>
2022-03-22 13:26:33 +0530

    initialize and setup firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "rsuite/dist/styles/rsuite-default.css";
+import "./styles/main.scss"
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      Hello
     </div>
   );
 }
---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-6KZbB5YxLLDXLkP71fAjlI2nVMSde8g",
+  authDomain: "react-chat-app-da396.firebaseapp.com",
+  projectId: "react-chat-app-da396",
+  storageBucket: "react-chat-app-da396.appspot.com",
+  messagingSenderId: "776399885762",
+  appId: "1:776399885762:web:ef5766db41e3717c9d325b"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
