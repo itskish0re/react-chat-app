05ba5f1a880d354b8d96663cd2315625c7dee0f7
Kishore Kumar <kishore1234500000@gmail.com>
2022-03-23 11:54:22 +0530

    add public and private routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Routes } from "react-router";
 import "rsuite/dist/styles/rsuite-default.css";
 import "./styles/main.scss"
+import PublicRoute from './components/PublicRoute';
+import PrivateRoute from './components/PrivateRoute';
+import SignIn from './pages/Signin';
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <Routes>
+      <PublicRoute path="/signin">
+        <SignIn/>
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home/>
+      </PrivateRoute>
+    </Routes>
   );
 }
 
