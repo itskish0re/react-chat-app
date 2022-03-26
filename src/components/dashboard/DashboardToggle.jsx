4b2f1893dc658bfc639c0d594dff5085b812c91b
Kishore Kumar <kishore1234500000@gmail.com>
2022-03-26 11:45:52 +0530

    create custom hook usemediaquery for responsiveness

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { useModalState } from '../../misc/customhooks';
+import { useModalState, useMediaQuery } from '../../misc/customhooks';
 import { Button, Drawer } from 'rsuite';
 import Dashboard from '.';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
+  const isMobile = useMediaQuery("(max-width: 992px)");
 
   return (
     <>
       <Button block color="blue" onClick={open}>
         Dashboard
       </Button>
-      <Drawer open={isOpen} onClose={close} placement="left">
+      <Drawer full={isMobile} open={isOpen} onClose={close} placement="left">
         <Dashboard />
       </Drawer>
     </>
--- src/misc/customhooks.jsx
+++ src/misc/customhooks.jsx
@@ -1,8 +1,26 @@
-import {useState, useCallback} from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 export const useModalState = (defaultValue = false) => {
   const [isOpen, setIsOpen] = useState(defaultValue);
   const open = useCallback(() => setIsOpen(true), []);
   const close = useCallback(() => setIsOpen(false), []);
   return { isOpen, open, close };
+};
+
+export const useMediaQuery = query => {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
+    const querList = window.matchMedia(query);
+    setMatches(querList.matches);
+
+    const listener = (e) => setMatches(e.matches);
+
+    querList.addEventListener("change",listener)
+    return () => querList.removeEventListener("change",listener)
+  }, [query]);
+
+  return matches;
 };
